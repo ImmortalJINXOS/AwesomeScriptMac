ce5e33fdaeb015867fa12591164a88422a46ba0f
JINXOS <jinxos@192.168.116.128>
2013-08-05 21:48:30 +0200

    Commit

--- AwesomeScriptMac/src/de/jinxos/AwesomeScript/MemoryWriter.java
+++ AwesomeScriptMac/src/de/jinxos/AwesomeScript/MemoryWriter.java
@@ -2,6 +2,11 @@ package de.jinxos.AwesomeScript;
 
 public class MemoryWriter {
 
+	public static int VM_READ = 1;
+	public static int VM_WRITE = 2;
+	public static int VM_EXECUTE = 4;
+	
 	public static native int CopyMemory(int task, int sourceptr, int size, int destinationptr);
 	public static native int GetTask(int pid);
+	public static native int ProtectMemory(int task, int address, int size, int protection);
 }
