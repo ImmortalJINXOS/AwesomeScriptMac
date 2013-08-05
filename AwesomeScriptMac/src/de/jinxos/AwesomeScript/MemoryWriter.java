a9d8bea1e5e610bcef8634b4e4f9c73f00c93e35
JINXOS <jinxos@192.168.116.128>
2013-08-05 16:59:59 +0200

    Commit

---
+++ AwesomeScriptMac/src/de/jinxos/AwesomeScript/MemoryWriter.java
@@ -0,0 +1,5 @@
+package de.jinxos.AwesomeScript;
+
+public class MemoryWriter {
+
+}
---
+++ AwesomeScriptMac/src/es/leyenda/AwesomeScript/Start.java
@@ -0,0 +1,10 @@
+package es.leyenda.AwesomeScript;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// This is your workspace, all the memory stuff is in de.jinxos.AwesomeScript
+
+	}
+
+}
